3ccb234b9c0006f5c64db6a2f64fef82554f24fc
Pradeepa Shanmugasundaram <pradeepa@Pradeepas-MacBook-Pro.local>
2019-10-30 13:00:42 -0400

    added some css

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,11 @@
 window.onload = function() {
-  document.getElementById("start-button").onclick = function() {
-    startGame();
-  };
 
-  function startGame() {
+//   document.getElementById("start-button").onclick = function() {
+//     startGame();
+//   };
 
-  }
+//   function startGame() {
+
+//   }
+
 };
